2cd182766f81279ccaf0a6b244e6d74af4c42e55
Jason Kurian <jasonk92@gmail.com>
2017-01-05 21:43:58 -0500

    uses each_char instead of chars.each

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -4,7 +4,7 @@ module Hamming
   def compute(strand_one, strand_two)
     raise ArgumentError unless strand_one.length == strand_two.length
 
-    strand_one.chars.each_with_index.reduce(0) do |distance, (char, i)|
+    strand_one.each_char.with_index.reduce(0) do |distance, (char, i)|
       distance += 1 unless char == strand_two[i]
       distance
     end
