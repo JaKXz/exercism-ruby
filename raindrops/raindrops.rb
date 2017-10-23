2151708b9dd5353036ca1f26e21bd8efdb9f7a8e
Jason Kurian <jasonk@nulogy.com>
2017-10-23 17:54:33 -0400

    first submission of raindrops

---
+++ raindrops/raindrops.rb
@@ -0,0 +1,30 @@
+module Raindrops
+  extend self
+
+  def convert(num)
+    factors_tuples.inject(num.to_s) do |result, (factor, response)|
+      if num % factor == 0
+        if result == num.to_s
+          result = response
+        else
+          result += response
+        end
+      end
+      result
+    end
+  end
+
+  private
+
+  def factors_tuples
+    [
+      [3, 'Pling'], 
+      [5, 'Plang'], 
+      [7, 'Plong']
+    ]
+  end
+end
+
+module BookKeeping
+  VERSION = 3
+end
